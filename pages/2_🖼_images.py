c9c8fa60a1ea8c57e2be35d291a650674320d95b
oseeJacques <oseesoke@gmail.com>
2023-07-20 14:03:02 +0100

    modify image interface

--- pages/2_🖼_images.py
+++ pages/2_🖼_images.py
@@ -13,7 +13,7 @@ test_data_dir = os.path.join(os.getcwd(), "src/testdata")
 with st.sidebar:
     st.sidebar.image(Image.open(os.path.join(os.getcwd(), "src/testdata/13.jpg")), use_column_width=True, width=st.sidebar.width)
 
-with open("E:/AllProject/AllProject/dronetrack/styles/style1.css") as css_source:
+with open(os.path.join(os.getcwd(), "styles/style1.css")) as css_source:
     st.markdown(f"<style>{css_source.read()}</style>", unsafe_allow_html=True)
 
     if not os.path.exists(test_data_dir):
